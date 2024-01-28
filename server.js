325f8608a749144385989555e6434e94d17fa478
Vera Maczali <maczalivera@gmail.com>
2024-01-28 12:01:58 +0100

    Add MongoDb and update routes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const port = 5000;
+require("dotenv").config();
+const port = process.env.port || 5000;
+const connectDB = require("./config/db");
 
+connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
